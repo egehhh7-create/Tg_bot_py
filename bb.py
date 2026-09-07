3a5a45dcaff7b2fc3ea2ec0a1da1219a2a6d1afa
egehhh7-create <egehhh7@gmail.com>
2026-09-07 21:39:12 +0600

    Update bb.py

--- bb.py
+++ bb.py
@@ -1,4 +1,4 @@
-import os
+জজimport os
 import threading
 from flask import Flask
 import logging
@@ -2495,5 +2495,5 @@ def main():
     app.run_polling()
 
 
-if name == "main":
+if__name__=="__main__":
     main()
